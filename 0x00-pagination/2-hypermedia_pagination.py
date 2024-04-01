4ac49f727bbe80b80abf8d8f3ee6129819be1558
Maurice <maurice.nzai@gmail.com>
2024-04-01 08:34:25 +0000

    Update Task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -4,7 +4,7 @@ Pagination helper function that determines the index range, page and page size
 """
 import csv
 import math
-from typing import  Dict, List, Tuple
+from typing import Dict, List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -44,9 +44,9 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         Retrieves  the correct indexes to paginate the dataset correctly
-        and return the appropriate page of the dataset 
+        and return the appropriate page of the dataset
         """
-        assert type(page) == int and type(page_size) == int
+        assert isinstance(page, int) and isinstance(page-size, int)
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset()
@@ -55,14 +55,14 @@ class Server:
         return data[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-         """
-         Retrieves info about a page
-         returns this info in a dictionary format           
-         """
-         page_data = self.get_page(page, page_size)
-         start, end = index_range(page, page_size)
-         total_pages = math.ceil(len(self.__dataset) / page_size)
-         page_info = {
+        """
+        Retrieves info about a page
+        returns this info in a dictionary format
+        """
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        page_info = {
                  'page_size': len(page_data),
                  'page': page,
                  'data': page_data,
@@ -70,4 +70,4 @@ class Server:
                  'prev_page': page - 1 if start > 0 else None,
                  'total_pages': total_pages,
                  }
-         return page_info
+        return page_info
