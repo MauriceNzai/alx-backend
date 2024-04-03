f6d5cf0fbfcb5a7e82f094d58a3b7a9d169d2474
Maurice <maurice.nzai@gmail.com>
2024-04-03 10:46:31 +0000

    Add Task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+LRUCache caching class
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    basic caching models an object that allows storage and
+    retrieval of items from a dictionary.
+    Lru caching system have limits, removes least used item
+    """
+
+    def __init__(self):
+        """
+        initialier constractor
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        adds item to the cache i.e. dictionary self.cache_data
+        if max size reached discard last put item
+        """
+        if key is None or item is None:
+            pass
+        if key not in self.cache_data:
+            # remove least used item if size exceeded
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                least_key, _ = self.cache_data.popitem(True)
+                print("DICARD:", least_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=False)
+
+    def get(self, key):
+        """
+        Gets an item from the cache dictionary in self.cache_data
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        # self.cache_data.get(key, last=False)
+        return self.cache_data.get(key)
