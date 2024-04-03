ceeaa8ae6a26250a83c6f81a788d24f38f38dcb3
Maurice <maurice.nzai@gmail.com>
2024-04-03 10:44:26 +0000

    Add Task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+LIFOCache caching class
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    basic caching models an object that allows storage and
+    retrieval of items from a dictionary.
+    Lifo caching system have limits, removes last put item
+    """
+
+    def __init__(self):
+        """
+        initialier constractor
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        adds item to the cache i.e. dictionary self.cache_data
+        if max size reached discard last put item
+        """
+        if key is None or item is None:
+            pass
+        if key not in self.cache_data:
+            # remove first put item if size exceeded
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DICARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """
+        Gets an item from the cache dictionary in self.cache_data
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
