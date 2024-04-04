28a7477d83b78a7f3b8cad8296791a3253ced6b8
Maurice <maurice.nzai@gmail.com>
2024-04-04 19:45:38 +0000

    Update Task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -28,8 +28,8 @@ class FIFOCache(BaseCaching):
             return
 
         # remove first put item if size exceeded
-        if len(self.cache_data) >
-        BaseCaching.MAX_ITEMS and key not in self.cache_data.keys():
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                and key not in self.cache_data.keys():
             first_key = next(iter(self.cache_data.keys()))
             del self.cache_data[first_key]
             print("DICARD: ", first_key)
