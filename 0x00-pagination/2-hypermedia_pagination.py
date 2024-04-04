89c67d0e63bdaa021328e74215f05106de775bf8
Maurice <maurice.nzai@gmail.com>
2024-04-04 23:32:14 +0000

    Update Task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -46,7 +46,7 @@ class Server:
         Retrieves  the correct indexes to paginate the dataset correctly
         and return the appropriate page of the dataset
         """
-        assert isinstance(page, int) and isinstance(page_size, int)
+        assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset()
