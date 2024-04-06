be9e846c6870893e15f011f9896b840612890bcd
Maurice <maurice.nzai@gmail.com>
2024-04-06 12:02:18 +0000

    Update Tasks 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -29,14 +29,13 @@ class LRUCache(BaseCaching):
             return
         if key not in self.cache_data:
             # remove least used item if size exceeded
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 least_key, _ = self.cache_data.popitem(True)
                 print("DISCARD:", least_key)
                 self.cache_data[key] = item
                 self.cache_data.move_to_end(key, last=False)
 
         self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=False)
 
     def get(self, key):
         """
